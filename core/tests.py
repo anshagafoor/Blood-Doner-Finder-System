db19996a9b165050ab5a6d25cb8378c17b5c56de
anshagafoor <anshaaashy@gmail.com>
2026-09-14 16:15:00 +0530

    test: expand integration test coverage across all workflow modules

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class SystemIntegrationTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page_status(self):
+        response = self.client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_status(self):
+        response = self.client.get(reverse('accounts:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_donor_search_accessible(self):
+        response = self.client.get(reverse('donors:search'))
+        self.assertEqual(response.status_code, 200)
